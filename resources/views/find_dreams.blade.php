979dcc778898be13247beb95cb81bbcb2c3bcd42
KodaiTakigawa <travelleritf777@gmail.com>
2018-06-03 17:03:45 +0900

    use blade template for navbar

--- resources/views/find_dreams.blade.php
+++ resources/views/find_dreams.blade.php
@@ -9,29 +9,13 @@
     <link rel="stylesheet" href="{{ asset('css/find_dreams.css') }}">
   </head>
   <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">BucketList</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="menu">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Dream">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    @include('layouts.navbar')
 
     <!-- dream -->
     <div class="d-flex justify-content-center">
-      <form class="form-inline">
-        <input class="form-control form-control-lg mr-sm-2" type="search" placeholder="Search" aria-label="Search" size="60%">
+      <form class="form-inline" action="/find-dreams" method="get">
+        {{ csrf_field() }}
+        <input class="form-control form-control-lg mr-sm-2" type="search" name="dreams" placeholder="Dream" aria-label="Search" size="60%">
         <button class="btn btn-outline-success button-control-lg my-2 my-sm-0" type="submit">Search</button>
       </form>
     </div>
--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -4,28 +4,12 @@
     <meta charset="utf-8">
     <title>BucketList-MyPage</title>
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
     <link rel="stylesheet" href="{{ asset('css/mypage.css') }}">
   </head>
   <body>
 <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">BucketList</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="menu">
-        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" type="search" placeholder="Dream">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-      </div>
-    </nav>
+    @include('layouts.navbar')
 
 <!-- profile -->
     <div class="container">
@@ -34,14 +18,16 @@
           <img src="img/profile.jpg" alt="Avatar" class="avatar">
         </div>
         <div class="col-sm-7">
-          <h1>瀧川滉大<br></h1>
-          <a class="btn btn-outline-primary" href="" role="button">Edit profile</a>
-          <p>旅行が好きで、将来は旅人になりたいです。<br></p>
-          <p>叶えた夢の数：77<br></p>
+          <h1>瀧川滉大</h1>
+          <p>旅行が好きで、将来は旅人になりたいです。</p>
+          <p>叶えた夢の数：77</p>
         </div>
       </div>
       <div class="container">
         <h2 class="list-name">LIST</h2>
+        <a href="/mypage/add-mydream"><div id="add" class="float-right">
+          <p>+</p>
+        </div></a>
       </div>
       <div class="container">
         <div class="row">
