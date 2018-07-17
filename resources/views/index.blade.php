cdf68919438d815f95db426be54692fab2239e47
vagrant <vagrant@homestead>
2018-07-17 07:56:20 +0000

    fix index about margin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,17 +10,19 @@
   <body>
     @include('layouts.navbar')
     <div class="d-flex flex-column">
-      <form class="form-inline form-search mx-auto" action="/find-dreams" method="get">
-        {{ csrf_field() }}
-        <div>
-          <input class="form-control input-search" type="search" name="search" placeholder="Dream" aria-label="Search">
+      <div class="mx-auto m-5">
+        <form class="form-inline form-search" action="/find-dreams" method="get">
+          {{ csrf_field() }}
+          <div>
+            <input class="form-control input-search" type="search" name="search" placeholder="Dream" aria-label="Search">
+          </div>
           <button class="btn btn-outline-success" type="submit">Search</button>
-        </div>
-      </form>
-      <div class="mx-auto">
+        </form>
+      </div>
+      <div class="mx-auto mb-5">
         <a class="btn btn-info social-login-btn" href="/login/twitter" role="button">Login with Twitter</a>
       </div>
-      <div class="mx-auto">
+      <div class="mx-auto mb-5">
         <h1>Dreamersとは・・・</h1>
         <p>夢や目標を持った人達が集まる場所です。<br></p>
         <p>夢や目標を登録して、仲間を見つけよう！</p>
