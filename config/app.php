6c9254b1ba669de6a8a7a5244d45e9b81bb5f1f1
vagrant <vagrant@homestead>
2018-07-18 01:48:59 +0000

    set url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://dreamersbonds.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://dreamersbonds.herokuapp.com'),
 
     /*
     |--------------------------------------------------------------------------
