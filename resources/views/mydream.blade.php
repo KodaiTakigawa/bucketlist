2c9047fe69a4e222715b4832b2d166c67ab8fb56
vagrant <vagrant@homestead>
2018-07-18 14:06:30 +0000

    add google analytics

--- resources/views/mydream.blade.php
+++ resources/views/mydream.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-122528519-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-122528519-1');
+    </script>
     <meta charset="utf-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{$dream->title}} -Dreamers-</title>
