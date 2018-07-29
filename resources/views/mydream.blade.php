1d296c67f958c3d3f01e8d5430f0524d1f5c3e5b
vagrant <vagrant@homestead>
2018-07-29 05:14:16 +0000

    Add URL when tweet first

--- resources/views/mydream.blade.php
+++ resources/views/mydream.blade.php
@@ -65,7 +65,7 @@
         <div class="media border border-info rounded bg-white mb-1 p-1">
             <div class="media-body">
               #{{$dream->title}} #DreamersをつけてTweetしよう！<br>
-              →<a href="https://twitter.com/intent/tweet" data-hashtags="{{$dream->title}},Dreamers" class="twitter-hashtag-button" data-show-count="false">Tweet #{{$dream->title}}</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+              →<a href="https://twitter.com/intent/tweet" data-hashtags="{{$dream->title}},Dreamers" data-url="{{ action('MainController@findDreamsDetail', ['id' => $dream->id]) }}" class="twitter-hashtag-button" data-show-count="false">Tweet #{{$dream->title}}</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
             </div>
         @endif
         @foreach($tweets_for_dream as $tweet)
